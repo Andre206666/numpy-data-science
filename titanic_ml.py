c22971683592cb303ed3b9e7109db59bad8f4087
LENOVO" Andrew <ap10222006@gmail.com>
2026-07-20 20:50:15 -0600

    add complete diabetes prediction system with feature engineering

--- titanic_ml.py
+++ titanic_ml.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.tree import DecisionTreeClassifier
+
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 print(df.head())
 print(df.shape)
